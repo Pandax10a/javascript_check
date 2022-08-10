ac29978e1c659526b7e90ef5141abbf335481982
panda1 <blueblue0011@gmail.com>
2022-08-09 18:52:15 -0700

    need to look into again

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <script-example/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScriptExample from '@/components/ScriptExample.vue';
+
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ScriptExample,
+  },
+};
+
+
 </script>
 
 <style>
---
+++ src/components/ScriptExample.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>testing</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                tweets: ['iam the best', 'first below', 'huh?'],
+                num_friends: 99,
+                logged_in: false,
+            }
+        },
+        name: 'script-example'
+    }
+</script>
+
+<style scoped>
+
+</style>
