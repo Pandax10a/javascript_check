b8b11b4de41236149be830a983e9dbd52f5cbf32
panda1 <blueblue0011@gmail.com>
2022-08-18 13:35:33 -0700

    going over knowledge check to understand variable, function, mounted

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <script-example/>
+    <script-example></script-example>
   </div>
 </template>
 
--- src/components/ScriptExample.vue
+++ src/components/ScriptExample.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
-        <h1>testing</h1>
+       <h1> testing </h1>
     </div>
 </template>
 
 <script>
+
     export default {
-        data: function () {
+                data: function () {
             return {
                 tweets: ['iam the best', 'first below', 'huh?'],
                 num_friends: 99,
                 logged_in: false,
             }
+        },
+        methods: {
+            show_info: function() {
+                document.body.insertAdjacentHTML(`beforeend`, `<h3>my num of friends: ${this.num_friends}</h3>
+                <h3>is logged in? ${this.logged_in}</h3>`)
+                for (let i = 0;i<this.tweets.length; i++) {
+                    document.body.insertAdjacentHTML(`beforeend`, `the tweet ${i+1}: ${this.tweets[i]} <br>`)
+                }
+
+    
+            
+            }
+        },
+
+        mounted () {
+            this.show_info();
+            
         },
         name: 'script-example'
     }
